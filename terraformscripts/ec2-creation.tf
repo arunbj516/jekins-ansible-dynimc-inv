f0734aff3aeb18dee0dcc3ad6f1c9ff996ba6609
arunbj516 <159615336+arunbj516@users.noreply.github.com>
2025-03-01 22:28:23 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,8 +5,8 @@ region = "ap-south-1"
 resource "aws_instance" "AWSEC2Instance"{
    ami = "ami-0d682f26195e9ec0f"
    instance_type = "t2.micro"
-   key_name = "devopsjan2025"
-   security_groups = ["launch-wizard-1"]
+   key_name = "aws"
+   security_groups = ["launch-wizard-9"]
    tags={
    Name = "terraformserver"
    }
